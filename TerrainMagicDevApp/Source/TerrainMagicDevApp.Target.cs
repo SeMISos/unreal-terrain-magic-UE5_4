f38074bfdda2f364a667d1b39e629e645d967a6e
Kacper <Kacper@SEMI-WIN-PC>
2026-02-05 11:32:20 +0100

    Added support for Unreal Engine 5.4.4

--- TerrainMagicDevApp/Source/TerrainMagicDevApp.Target.cs
+++ TerrainMagicDevApp/Source/TerrainMagicDevApp.Target.cs
@@ -8,7 +8,7 @@ public class TerrainMagicDevAppTarget : TargetRules
 	public TerrainMagicDevAppTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 		ExtraModuleNames.Add("TerrainMagicDevApp");
 	}
 }
